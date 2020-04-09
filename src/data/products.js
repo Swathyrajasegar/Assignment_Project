3f1cdf32fb5f7c48c243696f1e844c229237f957
Swathyrajasegar <51398822+Swathyrajasegar@users.noreply.github.com>
2020-04-09 12:56:10 +0530

    uploaded

---
+++ src/data/products.js
@@ -0,0 +1,46 @@
+const data = [
+  {
+    id: 1,
+    name: 'Idly',
+    price: 40,
+    currency: 'INR',
+    image: 'images/01.jpg',
+  },
+  {
+    id: 2,
+    name: 'Chappati',
+    price: 60,
+    currency: 'INR',
+    image: 'images/02.jpg',
+  },
+  {
+    id: 4,
+    name: 'Biriyani',
+    price: 120,
+    currency: 'INR',
+    image: 'images/04.jpg',
+  },
+  {
+    id: 6,
+    name: 'Parotta',
+    price: 100,
+    currency: 'INR',
+    image: 'images/06.jpg',
+  },
+  {
+    id: 5,
+    name: 'Naan',
+    price: 150,
+    currency: 'INR',
+    image: 'images/05.jpg',
+  },
+  {
+    id: 3,
+    name: 'Noodles',
+    price: 190,
+    currency: 'INR',
+    image: 'images/03.jpg',
+  }
+];
+
+export default data;
